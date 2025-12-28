809fb88d2d49ca7a145e974349896cdeffaf841f
Tatiana Nikolaeva <tatiana.nikolaeva@gmx.net>
2025-12-28 23:31:31 +0100

    postgres user store and tests

---
+++ apps/auth-service/internal/storage/postgres/user_store.go
@@ -0,0 +1,102 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/Tata-Matata/family-space/apps/auth-service/internal/domain"
+	errs "github.com/Tata-Matata/family-space/apps/auth-service/internal/errors"
+	"github.com/Tata-Matata/family-space/apps/auth-service/internal/storage"
+)
+
+// The exact type of attached sql executor (sql.DB, sql.Tx etc)
+// defines how the store will perform sql operations - in a transaction or not;
+// this decision is made on the service layer (not on the store layer)
+type UserStore struct {
+	sql storage.SQLExecutor
+}
+
+func NewUserStore(sqlExec storage.SQLExecutor) storage.UserStore {
+	return &UserStore{sql: sqlExec}
+}
+
+func (store *UserStore) Create(ctx context.Context, user domain.User) error {
+	const q = `
+		INSERT INTO users (
+			id,
+			email,
+			password_hash,
+			created_at
+		)
+		VALUES ($1, $2, $3, $4)
+	`
+
+	_, err := store.sql.ExecContext(
+		ctx,
+		q,
+		user.ID,
+		user.Email,
+		user.PasswordHash,
+		user.CreatedAt,
+	)
+
+	return err
+}
+
+func (store *UserStore) GetByEmail(
+	ctx context.Context,
+	email string,
+) (domain.User, error) {
+
+	const query = `
+		SELECT
+			id,
+			email,
+			password_hash,
+			created_at
+		FROM users
+		WHERE email = $1
+	`
+
+	var user domain.User
+	err := store.sql.QueryRowContext(ctx, query, email).
+		Scan(&user.ID, &user.Email, &user.PasswordHash, &user.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return domain.User{}, errs.ErrNotFound
+		}
+		return domain.User{}, err
+	}
+
+	return user, nil
+}
+
+func (store *UserStore) GetById(
+	ctx context.Context,
+	id string,
+) (domain.User, error) {
+
+	const query = `
+		SELECT
+			id,
+			email,
+			password_hash,
+			created_at
+		FROM users
+		WHERE id = $1
+	`
+
+	var user domain.User
+	err := store.sql.QueryRowContext(ctx, query, id).
+		Scan(&user.ID, &user.Email, &user.PasswordHash, &user.CreatedAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return domain.User{}, errs.ErrNotFound
+		}
+		return domain.User{}, err
+	}
+
+	return user, nil
+}
